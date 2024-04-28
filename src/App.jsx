667785d875e7c47a380af49f6ba72303d0e27b8d
MAG <gabriellimiguel01@gmail.com>
2024-04-27 21:35:45 -0300

    subido el login perros

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import "../css/titulo.css"
+import "../css/title.css"
+import "../css/login.css"
 
 
 function App() {
   return (
-    <div>
-      <h1>Micro-Emprendimientos</h1>
-    </div>
+    <>
+      <h1 className="title">Micro-Emprendimientos</h1>
+      <Login />
+    </>
   
   )
 }
+
+function Login() {
+  return(
+    <>
+    <div className='login'>
+      <h5>Ingresar</h5>
+      <input className='controles' type="text" name='usuario'  placeholder='Usuario' />
+      <input className='controles' type="password" name='password'  placeholder='Password' />
+      <input className='boton' type="submit" name='' placeholder='Auth' formTarget='_blank'/>
+      <p><a href="#">forgot your password?</a></p>
+    </div>
+    </>
+  );
+  
+}
+
 
 export default App
