851845ba76857c4e99b813c817a44f76a7666b83
Lucas_Aguilar <133165579+LucasAguilar647@users.noreply.github.com>
2024-05-04 01:53:33 -0300

    meti la pantalla de emprendimeintos y una lista

---
+++ assets/ListaDeEmprendimientos.jsx
@@ -0,0 +1,20 @@
+export const emprendimientos = [
+    "Tienda en línea de productos artesanales",
+    "Consultoría en marketing digital",
+    "Servicio de desarrollo de aplicaciones móviles",
+    "Restaurante de comida saludable",
+    "Agencia de viajes especializada en ecoturismo",
+    "Plataforma de educación en línea",
+    "Tienda de ropa vintage",
+    "Servicio de diseño de páginas web",
+    "Empresa de diseño gráfico",
+    "Cafetería temática de libros",
+    "Estudio de yoga y meditación",
+    "Servicio de consultoría financiera",
+    "Empresa de delivery de alimentos orgánicos",
+    "Plataforma de alquiler de bicicletas eléctricas",
+    "Servicio de reparación de dispositivos electrónicos"
+  ];
+  
+
+  
--- components/PantallaPrincipal.jsx
+++ components/PantallaPrincipal.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React from "react";
+import "../css/pantallaPrincipal.css";
+import { emprendimientos } from  "../assets/ListaDeEmprendimientos"
 
 export const PantallaPrincipal = () => {
+  
 
-
-    
   return (
-    <div>PantallaPrincipal</div>
-  )
-}
+    <div className="container">
+      <section className="search-section">
+        <h1>Busca Emprendimientos</h1>
+        <div className="search-bar">
+          <input type="text" placeholder="Buscar emprendimientos..." />
+          <button>Buscar</button>
+        </div>
+        <section className="emprendimientos-section">
+          <button>Registrar Emprendimiento</button>
+        </section>
+        <div className="emprendimientos-list">
+          <h2>Lista de Emprendimientos</h2>
+          <ol>
+            {emprendimientos.map((emprendimiento, index) => (
+              <li key={index}>{emprendimiento}</li>
+            ))}
+          </ol>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default PantallaPrincipal;
