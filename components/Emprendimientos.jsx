56479b350dcafa331e872ec7cb81d616295a509c
Lucas_Aguilar <133165579+LucasAguilar647@users.noreply.github.com>
2024-05-04 11:58:03 -0300

    le agregue un boton para ver el detalle del emprendimeinto

--- components/Emprendimientos.jsx
+++ components/Emprendimientos.jsx
@@ -2,11 +2,19 @@ import React from "react";
 import listaEmprendimientos from '../assets/ListaDeEmprendimientos'
 
 const Emprendimientos = () => {
+
+    const handleVerDetalle = (index)=> {
+        console.log(listaEmprendimientos[index] )
+    }
+
   return (
     <>
       <ol>
         {listaEmprendimientos.map((emprendimiento, index) => (
-          <li key={index}>{emprendimiento}</li>
+          <li key={index}>
+            {emprendimiento}
+            <button onClick={() => handleVerDetalle(index)} >Ver detalle</button> 
+          </li>
         ))}
       </ol>
     </>
