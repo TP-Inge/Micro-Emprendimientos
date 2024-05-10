158cc8c185a2050af7d0c881556ea0b26ae6bc83
Lucas_Aguilar <133165579+LucasAguilar647@users.noreply.github.com>
2024-05-09 22:04:14 -0300

    obtengo los datos desde un json

--- src/components/VerDetalle.jsx
+++ src/components/VerDetalle.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import listaEmprendimientos from '../assets/ListaDeEmprendimientos'
+import axios from 'axios'
 
-const VerDetalle = ( {emprendimiento} ) => {
+const VerDetalle = (   ) => {
     
+  const [emprendimiento, setEmprendimiento] = useState(null)
+  const [id, setId] = useState(1)
+
+
+  useEffect(() => {
+    axios.get("../../data.json")
+    .then(function(response){
+      const empre = response.data.emprendimientos.find((e)=>e.id === id.toString())
+      setEmprendimiento(empre)
+    })
+    .catch(function(error){
+      console.log("error de mierda")
+    })
+  }, [id])
+  
+
+
   return (
     <>
-       <h2> {emprendimiento} </h2>
-       <p> descripcion </p>
-        
-
+      <h1> {emprendimiento? emprendimiento.nombre :" CARGANDO..."} </h1>
     </>
   )
 }
