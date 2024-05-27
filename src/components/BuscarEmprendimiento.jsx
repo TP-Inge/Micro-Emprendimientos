a6edd0f75a9074c65003388e20aca038d80ba04a
Lucas_Aguilar <133165579+LucasAguilar647@users.noreply.github.com>
2024-05-27 12:04:20 -0300

    agregue un poco de css

--- src/components/BuscarEmprendimiento.jsx
+++ src/components/BuscarEmprendimiento.jsx
@@ -1,25 +1,26 @@
-import "../css/buscarEmprendimiento.css"
+import React from "react";
+import "../css/buscarEmprendimiento.css";
 
+export const BuscarEmprendimiento = ({ setMostrarFormulario }) => {
+  const handleRegistrar = () => {
+    setMostrarFormulario(true);
+  };
 
-export const BuscarEmprendimiento = ({setMostrarFormulario} ) => {
-    
-
-    const handleRegistrar = () => {
-      setMostrarFormulario(true);
-    };
-  
   return (
     <>
-        <h1>Busca Emprendimientos</h1>
-          <button id="register_button" onClick={handleRegistrar}>
-              Registrar Emprendimiento
-            </button>
-            <div className="search-bar">
-              <input type="text" placeholder="Buscar emprendimientos..." />
-              <button>Buscar</button>
-            </div>
-            <section className="emprendimientos-section"></section>
-            <h2>Lista de Emprendimientos</h2>
-          </>
-  )
-}
+      <div>
+        <button id="register_button" onClick={handleRegistrar}>
+          Registrar Emprendimiento
+        </button>
+      </div>
+      <div className="search-bar">
+        <input type="text" placeholder="Buscar emprendimientos..." />
+        <button>Buscar</button>
+      </div>
+      <section className="emprendimientos-section"></section>
+      <h2>Lista de Emprendimientos</h2>
+    </>
+  );
+};
+
+export default BuscarEmprendimiento;
