161c345f8e01ca78ae73a6040f83ff704102e672
Lucas_Aguilar <133165579+LucasAguilar647@users.noreply.github.com>
2024-05-30 16:49:16 -0300

    loa rrelge

--- src/components/Emprendimientos.jsx
+++ src/components/Emprendimientos.jsx
@@ -37,8 +37,8 @@ export const Emprendimientos = ({ setMostrarFormulario }) => {
         <VerDetalle
           nombreEmprendimiento={emprendimientos[indiceEmprendimiento].nombre}
           descripcionEmprendimiento={emprendimientos[indiceEmprendimiento].descripcion}
-          coordenadaX={emprendimientos[indiceEmprendimiento].coordenada.x}
-          coordenadaY={emprendimientos[indiceEmprendimiento].coordenada.y}
+          coordenadaX={emprendimientos[indiceEmprendimiento].coordenadas.x}
+          coordenadaY={emprendimientos[indiceEmprendimiento].coordenadas.y}
           ubicacionDisponible={emprendimientos[indiceEmprendimiento].ubicacionDisponible}
           correo={emprendimientos[indiceEmprendimiento].correo}
           setVerDetalle={setVerDetalle}
