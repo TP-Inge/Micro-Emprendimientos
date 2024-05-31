d64fb97d8e48cf65a6798d02ea2eb26e557cc6b9
Lucas_Aguilar <133165579+LucasAguilar647@users.noreply.github.com>
2024-05-31 12:20:03 -0300

    modularice

--- src/components/RegistrarForm.jsx
+++ src/components/RegistrarForm.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { NormalizarUbicaciones } from "./NormalizarUbicaciones";
 import "../css/registroForm.css";
 import DibujarMapa from "./DibujarMapa";
+import FormComercio from "./FormComercio";
 
 const RegistrarForm = ({ setMostrarFormulario }) => {
   const [nombre, setNombre] = useState("");
@@ -115,8 +116,8 @@ const RegistrarForm = ({ setMostrarFormulario }) => {
     <>
       <div className="container">
         {comercioDisponible && (
-          <Form_comercio
-            set_nombre_comercio={setNombreComercio}
+          <FormComercio
+            setNombreComercio={setNombreComercio}
             setDireccionComercio={setDireccionComercio}
             setDescripcionGeneral={setDescripcionGeneral}
             setRubroComercio={setRubroComercio}
@@ -253,115 +254,6 @@ const RegistrarForm = ({ setMostrarFormulario }) => {
             position={[coordenada.y, coordenada.x]}
             nombreEmprendimiento={nombreEmprendimiento}
             ubicacionValidada={ubicacionValidada}
-          />
-        </div>
-      </div>
-    </>
-  );
-};
-
-const Form_comercio = ({
-  set_nombre_comercio,
-  setDireccionComercio,
-  setDescripcionGeneral,
-  setRubroComercio,
-  setContactoComercio,
-  setFormasPago,
-  setRedesSociales,
-  setRestricciones,
-  setZonaInfluencia,
-}) => {
-  return (
-    <>
-      <div className="form-comercio-container">
-        <div>
-          <label htmlFor="nombreComercio" required>
-            Nombre del Comercio:
-          </label>
-          <input
-            type="text"
-            id="nombreComercio"
-            onChange={(e) => set_nombre_comercio(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="direccionComercio" required>
-            Dirección del Comercio:
-          </label>
-          <input
-            type="text"
-            id="direccionComercio"
-            onChange={(e) => setDireccionComercio(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="descripcionGeneral" required>
-            Descripción general:
-          </label>
-          <input
-            type="text"
-            id="descripcionGeneral"
-            onChange={(e) => setDescripcionGeneral(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="rubroComercio" required>
-            Rubro:
-          </label>
-          <input
-            type="text"
-            id="rubroComercio"
-            onChange={(e) => setRubroComercio(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="contactoComercio" required>
-            Forma de contacto:
-          </label>
-          <input
-            type="text"
-            id="contactoComercio"
-            onChange={(e) => setContactoComercio(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="formasPago" required>
-            Formas de pago:
-          </label>
-          <input
-            type="text"
-            id="formasPago"
-            onChange={(e) => setFormasPago(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="redesSociales" required>
-            Redes sociales:
-          </label>
-          <input
-            type="text"
-            id="redesSociales"
-            onChange={(e) => setRedesSociales(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="restricciones" required>
-            Restricciones:
-          </label>
-          <input
-            type="text"
-            id="restricciones"
-            onChange={(e) => setRestricciones(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="zonaInfluencia" required>
-            Zona de influencia:
-          </label>
-          <input
-            type="text"
-            id="zonaInfluencia"
-            onChange={(e) => setZonaInfluencia(e.target.value)}
           />
         </div>
       </div>
