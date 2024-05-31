54a781af38c2ebfac22f5b6ccb462dcf5aa18486
Lucas_Aguilar <133165579+LucasAguilar647@users.noreply.github.com>
2024-05-31 11:54:30 -0300

    arregle lo de ubicacion validad

--- src/components/RegistrarForm.jsx
+++ src/components/RegistrarForm.jsx
@@ -1,34 +1,30 @@
-import React, { useState } from 'react';
-import { NormalizarUbicaciones } from './NormalizarUbicaciones'
+import React, { useEffect, useState } from "react";
+import { NormalizarUbicaciones } from "./NormalizarUbicaciones";
 import "../css/registroForm.css";
-import DibujarMapa from './DibujarMapa';
-
-
-
+import DibujarMapa from "./DibujarMapa";
 
 const RegistrarForm = ({ setMostrarFormulario }) => {
-  const [nombre, setNombre] = useState('');
-  const [telefono, setTelefono] = useState('');
-  const [correo, setCorreo] = useState('');
-  const [nombreEmprendimiento, setNombreEmprendimiento] = useState('');
-  const [descripcionEmprendimiento, setDescripcionEmprendimiento] = useState('');
-  const [ubicacionEmprendimiento, setUbicacionEmprendimiento] = useState('');
-  const [vinculoOrganizacion, setVinculoOrganizacion] = useState('');
+  const [nombre, setNombre] = useState("");
+  const [telefono, setTelefono] = useState("");
+  const [correo, setCorreo] = useState("");
+  const [nombreEmprendimiento, setNombreEmprendimiento] = useState("");
+  const [descripcionEmprendimiento, setDescripcionEmprendimiento] = useState("");
+  const [ubicacionEmprendimiento, setUbicacionEmprendimiento] = useState("");
+  const [vinculoOrganizacion, setVinculoOrganizacion] = useState("");
   const [ubicacionDisponible, setUbicacionDisponible] = useState(true);
-  const [ubicacionValidada, setUbicacionValidada] = useState(false)
+  const [ubicacionValidada, setUbicacionValidada] = useState(false);
   const [coordenada, setCoordenada] = useState({ x: -58.3816, y: -34.6037 });
   const [comercioDisponible, set_comercio_disponible] = useState(false);
 
-  const [nombreComercio,set_nombre_comercio] = useState('');
-  const [direccionComercio, setDireccionComercio] = useState('');
-  const [descripcionGeneral, setDescripcionGeneral] = useState('');
-  const [rubroComercio, setRubroComercio] = useState('');
-  const [contactoComercio, setContactoComercio] = useState('');
-  const [formasPago, setFormasPago] = useState('');
-  const [redesSociales, setRedesSociales] = useState('');
-  const [restricciones, setRestricciones] = useState('');
-  const [zonaInfluencia, setZonaInfluencia] = useState('');
-
+  const [nombreComercio, set_nombre_comercio] = useState("");
+  const [direccionComercio, setDireccionComercio] = useState("");
+  const [descripcionGeneral, setDescripcionGeneral] = useState("");
+  const [rubroComercio, setRubroComercio] = useState("");
+  const [contactoComercio, setContactoComercio] = useState("");
+  const [formasPago, setFormasPago] = useState("");
+  const [redesSociales, setRedesSociales] = useState("");
+  const [restricciones, setRestricciones] = useState("");
+  const [zonaInfluencia, setZonaInfluencia] = useState("");
 
   var direcciones;
   const handleValidar = async (e) => {
@@ -37,80 +33,89 @@ const RegistrarForm = ({ setMostrarFormulario }) => {
       direcciones = await NormalizarUbicaciones(ubicacionEmprendimiento);
 
       if (direcciones.length === 0) {
-        alert('Debe ingresar una dirección válida.');
+        alert("Debe ingresar una dirección válida.");
         return;
       }
 
       const coordenadas = {
         x: direcciones.x,
-        y: direcciones.y
+        y: direcciones.y,
       };
 
       setCoordenada(coordenadas);
-
-
+      setUbicacionValidada(true);
     } catch (error) {
-      if (error.message === 'Debe ingresar la altura en la dirección.') {
-        alert('Debe ingresar una altura.');
-      } else if (error.message === 'Debe ingresar el código de partido en la dirección.') {
-        alert('Debe ingresar el código de partido en la dirección.');
-      } else if (error.message === 'Debe ser más específico en la ubicación.') {
-        alert('Debe ser más específico en la ubicación.');
+      setUbicacionValidada(false)
+      if (error.message === "Debe ingresar la altura en la dirección.") {
+        alert("Debe ingresar una altura.");
+      } else if (
+        error.message === "Debe ingresar el código de partido en la dirección."
+      ) {
+        alert("Debe ingresar el código de partido en la dirección.");
+      } else if (error.message === "Debe ser más específico en la ubicación.") {
+        alert("Debe ser más específico en la ubicación.");
       } else {
-        console.error('Error al normalizar la ubicación:', error);
-        alert('Error al normalizar la ubicación. Por favor, intente nuevamente.');
+        console.error("Error al normalizar la ubicación:", error);
+        alert(
+          "Error al normalizar la ubicación. Por favor, intente nuevamente."
+        );
       }
     }
-    setUbicacionValidada(true)
-
   };
 
-  var handleSubmit = () => {
+  const handleSubmit = () => {
+    if(ubicacionValidada && ubicacionEmprendimiento!==''){
+        const nuevoEmprendimiento = {
+          id: Date.now().toString(),
+          nombre: nombreEmprendimiento,
+          telefono: telefono,
+          correo: correo,
+          descripcion: descripcionEmprendimiento,
+          ubicacion: ubicacionEmprendimiento,
+          vinculoOrganizacion: vinculoOrganizacion,
+          coordenada: coordenada,
+          ubicacionDisponible: ubicacionDisponible,
+    
+          nombreComercio: nombreComercio,
+          direccionComercio: direccionComercio,
+          descripcionGeneral: descripcionGeneral,
+          rubroComercio: rubroComercio,
+          contactoComercio: contactoComercio,
+          formasPago: formasPago,
+          redesSociales: redesSociales,
+          restricciones: restricciones,
+          zonaInfluencia: zonaInfluencia,
+        };
+    
+        const localStorageData = localStorage.getItem("emprendimientos");
+        const emprendimientos = localStorageData
+          ? JSON.parse(localStorageData)
+          : [];
+    
+        emprendimientos.push(nuevoEmprendimiento);
+    
+        const jsonData = JSON.stringify(emprendimientos);
+        localStorage.setItem("emprendimientos", jsonData);
+        setMostrarFormulario(false)
+    }else{
+      alert('Debe validar la ubicacion')
+    }
+  };
 
-
-    const nuevoEmprendimiento = {
-      id: Date.now().toString(),
-      nombre: nombreEmprendimiento,
-      telefono: telefono,
-      correo: correo,
-      descripcion: descripcionEmprendimiento,
-      ubicacion: ubicacionEmprendimiento,
-      vinculoOrganizacion: vinculoOrganizacion,
-      coordenada: coordenada,
-      ubicacionDisponible: ubicacionDisponible,
-
-      nombreComercio: nombreComercio,
-      direccionComercio: direccionComercio,
-      descripcionGeneral: descripcionGeneral,
-      rubroComercio: rubroComercio,
-      contactoComercio: contactoComercio,
-      formasPago: formasPago,
-      redesSociales: redesSociales,
-      restricciones: restricciones,
-      zonaInfluencia: zonaInfluencia
-
-    };
-
-    const localStorageData = localStorage.getItem('emprendimientos');
-    const emprendimientos = localStorageData ? JSON.parse(localStorageData) : [];
-
-    emprendimientos.push(nuevoEmprendimiento);
-
-    const jsonData = JSON.stringify(emprendimientos);
-    localStorage.setItem('emprendimientos', jsonData);
-
-  }
-
+  useEffect(() => {
+    setUbicacionValidada(false)
+  }, [ubicacionEmprendimiento])
+  
 
   const handleCancelarRegistro = () => {
-    setMostrarFormulario(false)
-  }
+    setMostrarFormulario(false);
+  };
 
   return (
     <>
-
       <div className="container">
-        {comercioDisponible && <Form_comercio 
+        {comercioDisponible && (
+          <Form_comercio
             set_nombre_comercio={set_nombre_comercio}
             setDireccionComercio={setDireccionComercio}
             setDescripcionGeneral={setDescripcionGeneral}
@@ -119,42 +124,95 @@ const RegistrarForm = ({ setMostrarFormulario }) => {
             setFormasPago={setFormasPago}
             setRedesSociales={setRedesSociales}
             setRestricciones={setRestricciones}
-            setZonaInfluencia={setZonaInfluencia}/>}
+            setZonaInfluencia={setZonaInfluencia}
+          />
+        )}
         <div className="form-container">
           <h2>Registrar Emprendimiento</h2>
-          <form>
+          <form onSubmit={handleSubmit} >
             <div>
-              <label htmlFor="nombreEmprendimiento">Nombre del Emprendimiento:</label>
-              <input type="text" id="nombreEmprendimiento" value={nombreEmprendimiento} onChange={(e) => setNombreEmprendimiento(e.target.value)} required />
+              <label htmlFor="nombreEmprendimiento">
+                Nombre del Emprendimiento:
+              </label>
+              <input
+                type="text"
+                id="nombreEmprendimiento"
+                value={nombreEmprendimiento}
+                onChange={(e) => setNombreEmprendimiento(e.target.value)}
+                required
+              />
             </div>
             <div>
               <label htmlFor="nombre">Nombre:</label>
-              <input type="text" id="nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} required />
+              <input
+                type="text"
+                id="nombre"
+                value={nombre}
+                onChange={(e) => setNombre(e.target.value)}
+                required
+              />
             </div>
             <div>
               <label htmlFor="telefono">Teléfono:</label>
-              <input type="tel" id="telefono" value={telefono} onChange={(e) => setTelefono(e.target.value)} required />
+              <input
+                type="tel"
+                id="telefono"
+                value={telefono}
+                onChange={(e) => setTelefono(e.target.value)}
+                required
+              />
             </div>
             <div>
               <label htmlFor="correo">Correo:</label>
-              <input type="email" id="correo" value={correo} onChange={(e) => setCorreo(e.target.value)} required />
+              <input
+                type="email"
+                id="correo"
+                value={correo}
+                onChange={(e) => setCorreo(e.target.value)}
+                required
+              />
             </div>
             <div>
-              <label htmlFor="descripcionEmprendimiento">Descripción del Emprendimiento:</label>
-              <textarea id="descripcionEmprendimiento" value={descripcionEmprendimiento} onChange={(e) => setDescripcionEmprendimiento(e.target.value)} required />
+              <label htmlFor="descripcionEmprendimiento">
+                Descripción del Emprendimiento:
+              </label>
+              <textarea
+                id="descripcionEmprendimiento"
+                value={descripcionEmprendimiento}
+                onChange={(e) => setDescripcionEmprendimiento(e.target.value)}
+                required
+              />
             </div>
             <div>
-              <label htmlFor="ubicacionEmprendimiento">Ubicación del Emprendimiento:</label>
-              <input type="text" id="ubicacionEmprendimiento" value={ubicacionEmprendimiento} onChange={(e) => setUbicacionEmprendimiento(e.target.value)} required />
-              <button onClick={handleValidar} > validar</button>
+              <label htmlFor="ubicacionEmprendimiento">
+                Ubicación del Emprendimiento:
+              </label>
+              <input
+                type="text"
+                id="ubicacionEmprendimiento"
+                value={ubicacionEmprendimiento}
+                onChange={(e) => setUbicacionEmprendimiento(e.target.value)}
+                required
+              />
+              <button onClick={handleValidar}> validar</button>
             </div>
             <div>
-              <label htmlFor="vinculoOrganizacion">Vínculo con la Organización:</label>
-              <input type="text" id="vinculoOrganizacion" value={vinculoOrganizacion} onChange={(e) => setVinculoOrganizacion(e.target.value)} required />
+              <label htmlFor="vinculoOrganizacion">
+                Vínculo con la Organización:
+              </label>
+              <input
+                type="text"
+                id="vinculoOrganizacion"
+                value={vinculoOrganizacion}
+                onChange={(e) => setVinculoOrganizacion(e.target.value)}
+                required
+              />
             </div>
-            <div className='container-checkbox'>
+            <div className="container-checkbox">
               <div>
-                <label htmlFor="ubicacionDisponible" id='ubi_disponible'>Ubicación Disponible:</label>
+                <label htmlFor="ubicacionDisponible" id="ubi_disponible">
+                  Ubicación Disponible:
+                </label>
                 <input
                   type="checkbox"
                   id="ubicacionDisponible"
@@ -162,22 +220,24 @@ const RegistrarForm = ({ setMostrarFormulario }) => {
                   onChange={(e) => setUbicacionDisponible(e.target.checked)}
                 />
               </div>
-              <label htmlFor="comercioDisponible" id="comercio_button">Tiene comercio:</label>
+              <label htmlFor="comercioDisponible" id="comercio_button">
+                Tiene comercio:
+              </label>
               <input
                 type="checkbox"
                 id="comercio_input"
                 checked={comercioDisponible}
-                onChange={
-                  (e) => set_comercio_disponible(e.target.checked)
-                }
+                onChange={(e) => set_comercio_disponible(e.target.checked)}
               />
-              <div>
-              </div>
-
+              <div></div>
             </div>
-            <div className=' buttons'>
-              <button type="submit" onClick={handleSubmit} >Registrarse</button>
-              <button type="cancelar" onClick={handleCancelarRegistro} >Cancelar registro</button>
+            <div className=" buttons">
+            {ubicacionValidada && (
+                <button type="submit">Registrarse</button>
+              )}
+              <button type="cancelar" onClick={handleCancelarRegistro}>
+                Cancelar registro
+              </button>
             </div>
           </form>
         </div>
@@ -187,18 +247,17 @@ const RegistrarForm = ({ setMostrarFormulario }) => {
             <h2> {nombreEmprendimiento} </h2>
             <p> {descripcionEmprendimiento} </p>
             <button>ver detalle</button>
-
           </div>
 
-
-          <DibujarMapa position={[coordenada.y, coordenada.x]} nombreEmprendimiento={nombreEmprendimiento} ubicacionValidada={ubicacionValidada} />
+          <DibujarMapa
+            position={[coordenada.y, coordenada.x]}
+            nombreEmprendimiento={nombreEmprendimiento}
+            ubicacionValidada={ubicacionValidada}
+          />
         </div>
-
       </div>
     </>
   );
-
-
 };
 
 const Form_comercio = ({
@@ -210,14 +269,15 @@ const Form_comercio = ({
   setFormasPago,
   setRedesSociales,
   setRestricciones,
-  setZonaInfluencia
+  setZonaInfluencia,
 }) => {
-  
   return (
     <>
-      <div className='form-comercio-container'>
+      <div className="form-comercio-container">
         <div>
-          <label htmlFor="nombreComercio" required>Nombre del Comercio:</label>
+          <label htmlFor="nombreComercio" required>
+            Nombre del Comercio:
+          </label>
           <input
             type="text"
             id="nombreComercio"
@@ -225,7 +285,9 @@ const Form_comercio = ({
           />
         </div>
         <div>
-          <label htmlFor="direccionComercio" required>Dirección del Comercio:</label>
+          <label htmlFor="direccionComercio" required>
+            Dirección del Comercio:
+          </label>
           <input
             type="text"
             id="direccionComercio"
@@ -233,7 +295,9 @@ const Form_comercio = ({
           />
         </div>
         <div>
-          <label htmlFor="descripcionGeneral" required>Descripción general:</label>
+          <label htmlFor="descripcionGeneral" required>
+            Descripción general:
+          </label>
           <input
             type="text"
             id="descripcionGeneral"
@@ -241,7 +305,9 @@ const Form_comercio = ({
           />
         </div>
         <div>
-          <label htmlFor="rubroComercio" required>Rubro:</label>
+          <label htmlFor="rubroComercio" required>
+            Rubro:
+          </label>
           <input
             type="text"
             id="rubroComercio"
@@ -249,7 +315,9 @@ const Form_comercio = ({
           />
         </div>
         <div>
-          <label htmlFor="contactoComercio" required>Forma de contacto:</label>
+          <label htmlFor="contactoComercio" required>
+            Forma de contacto:
+          </label>
           <input
             type="text"
             id="contactoComercio"
@@ -257,7 +325,9 @@ const Form_comercio = ({
           />
         </div>
         <div>
-          <label htmlFor="formasPago" required>Formas de pago:</label>
+          <label htmlFor="formasPago" required>
+            Formas de pago:
+          </label>
           <input
             type="text"
             id="formasPago"
@@ -265,7 +335,9 @@ const Form_comercio = ({
           />
         </div>
         <div>
-          <label htmlFor="redesSociales" required>Redes sociales:</label>
+          <label htmlFor="redesSociales" required>
+            Redes sociales:
+          </label>
           <input
             type="text"
             id="redesSociales"
@@ -273,7 +345,9 @@ const Form_comercio = ({
           />
         </div>
         <div>
-          <label htmlFor="restricciones" required>Restricciones:</label>
+          <label htmlFor="restricciones" required>
+            Restricciones:
+          </label>
           <input
             type="text"
             id="restricciones"
@@ -281,7 +355,9 @@ const Form_comercio = ({
           />
         </div>
         <div>
-          <label htmlFor="zonaInfluencia" required>Zona de influencia:</label>
+          <label htmlFor="zonaInfluencia" required>
+            Zona de influencia:
+          </label>
           <input
             type="text"
             id="zonaInfluencia"
@@ -289,12 +365,8 @@ const Form_comercio = ({
           />
         </div>
       </div>
-
     </>
   );
-}
-
-
-
+};
 
 export default RegistrarForm;
